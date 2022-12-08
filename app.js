3fa00df0790336e56932a38c55102c0e278a2394
lvuk <luka.vuk456@gmail.com>
2022-12-08 17:48:52 +0100

    connected to db

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
 
 //middleware
 app.use(express.json({}));
@@ -9,6 +11,17 @@ app.use(express.json({}));
 //routes
 app.use('/api/v1/tasks', tasks);
 
-app.listen(port, () => {
-  console.log('Server is started on 3000');
-});
+//server & db
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log('Successfully connected to db...');
+    app.listen(port, () => {
+      console.log('Server is started on 3000');
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
